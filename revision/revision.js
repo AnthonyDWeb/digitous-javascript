49a139d65080ab334d806c0bca74b2b4fdb99b4e
AnthonyDWeb <you@example.comdelforge.anthony@gmail.com>
2021-06-23 23:05:05 +0200

    transfert

---
+++ revision/revision.js
@@ -0,0 +1,21 @@
+// 01 Alphabet
+
+function sortletters(string){
+    var letter = [string];
+    letter = string.split("");
+    letter.sort();
+    letter = letter.join("");
+    console.log(letter);
+}
+
+sortletters("konexio")
+
+// 02 XoXo
+
+// function countEach(a, b){
+//     if ( a ==="x"){
+
+//     } else if (b ==="o") {
+
+//     }
+// }
